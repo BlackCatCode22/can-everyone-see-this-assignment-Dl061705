42b91ba9ea39d316c313eba3ce7f70f4c77f72ee
Dl061705 <157327265+Dl061705@users.noreply.github.com>
2024-01-21 18:36:09 -0800

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+Main sayhola () {
+    cout << "\n\n this hola is from sayhola()"
+    
+    
+    
+int main() {
+	cout << "\n\n Hola Mundo! \n\n";
+
+
+
+
+
+
+
+int numl;
+string mystr = "this is a string";
+
+
+num1 = 55;
+
+cout << " \n num1 is ... " << num1 << "\n\n";
+cout << " \n mystr is ... " << mystr << "\n\n";
+
+sayhola();
+return 0;
+}
